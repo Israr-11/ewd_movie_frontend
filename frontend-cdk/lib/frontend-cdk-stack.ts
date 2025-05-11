5968806fe46d42d25a71187c31515ead615485cc
Israr Ahmed <israrahmedpk1999@gmail.com>
2025-05-11 09:25:26 +0100

    cdk configured

--- frontend-cdk/lib/frontend-cdk-stack.ts
+++ frontend-cdk/lib/frontend-cdk-stack.ts
@@ -2,10 +2,13 @@ import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as path from 'path';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class FrontendCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -22,38 +25,75 @@ export class FrontendCdkStack extends Stack {
       validation: acm.CertificateValidation.fromDns(hostedZone),
     });
 
+    // Create an S3 bucket for website content
     const siteBucket = new s3.Bucket(this, 'SiteBucket', {
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-      publicReadAccess: false,
+      // Remove websiteIndexDocument and websiteErrorDocument properties
     });
 
-    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
-      originConfigs: [
+    // Create CloudFront Origin Access Control
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'OAC', {
+      originAccessControlConfig: {
+        name: 'OAC for S3',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      }
+    });
+
+    // Create a CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: [domainName],
+      certificate: certificate,
+      defaultRootObject: 'index.html',
+      errorResponses: [
         {
-          s3OriginSource: {
-            s3BucketSource: siteBucket,
-          },
-          behaviors: [{ isDefaultBehavior: true }],
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
         },
       ],
-      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
-        aliases: [domainName],
-        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
-        sslMethod: cloudfront.SSLMethod.SNI,
-      }),
     });
 
+    // Apply the OAC to the CloudFront distribution
+    const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
+    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity', '');
+    cfnDistribution.addPropertyOverride('DistributionConfig.Origins.0.OriginAccessControlId', oac.attrId);
+
+    // Grant CloudFront access to the S3 bucket
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        resources: [siteBucket.arnForObjects('*')],
+        conditions: {
+          StringEquals: {
+            'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`
+          }
+        }
+      })
+    );
+
+    // Create a Route53 alias record for the CloudFront distribution
     new route53.ARecord(this, 'SiteAliasRecord', {
       recordName: domainName,
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
       zone: hostedZone,
     });
 
+    // Determine the correct path to the frontend build directory
+    const distPath = path.resolve(__dirname, '../../dist');
+
+    // Deploy the website content to the S3 bucket
     new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
-      sources: [s3deploy.Source.asset('../../ewd-movie-frontend/dist')],
+      sources: [s3deploy.Source.asset(distPath)],
       destinationBucket: siteBucket,
       distribution,
       distributionPaths: ['/*'],
